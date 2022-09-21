b79f3ffa8bdfe653e7bbb3126efb3107c21e4dfe
Richelle Joy Catabian <richellecatabian2gmail.com>
2022-09-20 18:27:21 -0700

    exercise

---
+++ exer2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PHP Exercise</title>
+</head>
+<body>
+ <h1>Exercise 2</h1>
+
+ <?php
+ 	$x=10;
+ 	$y=7;
+
+		$result=$x+$y; 
+		echo "$x + $y = $result<br />";   
+ 
+		$result=$x-$y; 
+		echo "$x - $y = $result<br />";   
+ 
+		$result=$x*$y; 
+		echo "$x * $y = $result<br />";   
+ 
+		$result=$x/$y; 
+		echo "$x / $y = $result<br />";   
+ 
+		$result=$x%$y; 
+		echo "$x % $y = $result<br />"; 
+
+
+ ?>
+</body>
+</html>
---
+++ twinkle.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> PHP Exercise</title>
+</head>
+<body>
+	<h1> Twinkle Litle Star</h1>
+<?php
+ 	echo "Twinkle, Twinkle little star. <br/>";
+ 	$twinkle="Twinkle"; 
+	$star="star"; 
+ 
+
+	echo "$twinkle, $twinkle little $star.<br/>";
+
+	$twinkle="Fly"; 
+	$star="bird"; 
+	echo "$twinkle, $twinkle little $star.";
+
+?>
+
+</body>
+</html>
